49a3961ff8c0678f7b7522a60ceab1ec09edc7ad
ValentinaGayoso <Valengayoso111@gmail.com>
2022-11-08 20:32:54 -0300

    Apertura de archivos

--- main/main.cpp
+++ main/main.cpp
@@ -1,10 +1,41 @@
 #include "iri.cpp"
 #include <iostream>
+#include <string>
+#include <fstream>
+#include <ctime>
+using namespace std;
 
 int main() {
+    fstream cons,cont,med,obs,pac;
+   
+    cons.open("IRI_Consultas.csv", ios::in);
+     if (!(cons.is_open()))
+            return -1;
 
-    std::cout << "Hello World TP_Final";
+    cont.open("IRI_Contactos.csv", ios::in);
+     if (!(cont.is_open()))
+            return -1;
 
+    med.open("IRI_Medicos.csv", ios::in);
+     if (!(med.is_open()))
+            return -1;
 
+    obs.open("IRI_ObraSocial.csv", ios::in);
+     if (!(obs.is_open()))
+            return -1;
+
+    pac.open("Pacientes.csv", ios::in);
+     if (!(pac.is_open()))
+            return -1;
+
+    //toda la vaina de leerlos 
+
+    cons.close();
+    cont.close();
+    med.close();
+    obs.close();
+    pac.close();
+
+    //seguimos con el codigo
     return EXIT_SUCCESS;
 }
