7cd57756cf6b4d946bdd95a41dbb2fc82baf948a
StephaniaSalvi <stephi091203@gmail.com>
2022-11-08 19:57:08 -0300

    agrego funcion contactar

---
+++ libreria/contactar.cpp
@@ -0,0 +1,10 @@
+#include "contactar.h"
+
+int Contactar()
+{
+    int aux;
+    cout << "Cual fue la respuesta del paciente?" << endl; //la respuesta la hace un random
+    srand(time(NULL));
+    aux = rand()% (4-1) + 1;
+    return aux;
+}
---
+++ libreria/contactar.h
@@ -0,0 +1,6 @@
+#include <string>
+#include <iostream>
+#include <ctime>
+using namespace std;
+
+int Contactar();
