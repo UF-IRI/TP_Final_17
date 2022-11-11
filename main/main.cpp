5e349af85f51dc585fc302bd46981a3f736a1e8e
StephaniaSalvi <stephi091203@gmail.com>
2022-11-11 13:54:24 -0300

    agrege el caso 3, update

--- main/main.cpp
+++ main/main.cpp
@@ -3,13 +3,11 @@
 #include <string>
 #include <fstream>
 #include <ctime>
-#include "resize.h"
-#include "contactar.h"
 using namespace std;
 
 //FALTA: 
-        //Funcion update --> solo obra social
-        //Funcion Archivar!! --> agregar etiqueta
+        //Funcion update --> preguntar como
+        //Funcion Archivar!!
         //Terminar de leer los archivos de obra social, consultas y medicos
         //Excepciones (no hay contacto, etc.)!
         //Que anden las structs :/ (esto saca todos los errores que aparecen)
@@ -21,26 +19,31 @@ using namespace std;
 //Variables mem estatica: i, aux, aux2, aux3, aux4, dummy, coma, 
 
 int main() {
-    fstream cons,cont,med,pac;
+    fstream cons,cont,med,obs,pac;
    
-    cons.open(BASE_PATH+"IRI_Consultas.csv", ios::in);
+    cons.open("IRI_Consultas.csv", ios::in);
      if (!(cons.is_open()))
             return -1;
 
-    cont.open(BASE_PATH+"IRI_Contactos.csv", ios::in);
+    cont.open("IRI_Contactos.csv", ios::in);
      if (!(cont.is_open()))
             return -1;
 
-    med.open(BASE_PATH+"IRI_Medicos.csv", ios::in);
+    med.open("IRI_Medicos.csv", ios::in);
      if (!(med.is_open()))
             return -1;
 
-    pac.open(BASE_PATH+"Pacientes.csv", ios::in);
+    obs.open("IRI_ObraSocial.csv", ios::in);
+     if (!(obs.is_open()))
+            return -1;
+
+    pac.open("Pacientes.csv", ios::in);
      if (!(pac.is_open()))
             return -1;
 
    
-    int N=20;
+    int *N = new int;
+    *N=20;
     int i = 0;
     int cantidad_aumentar = 5;
     
@@ -52,52 +55,74 @@ int main() {
     >> dummy >> coma >> dummy; //leo el header de Pacientes.csv
 
     while(pac){
-        if(i==N-1){
-                resize(array, &N, cantidad_aumentar); 
+        if(i==*N-1){
+                resize(Paciente *&array, *N, cantidad_aumentar); 
         }
         
-        pac >> array[i].DNI >> coma >> array[i].nombre >> coma >> array[i].apellido >> coma 
-        >> array[i].sexo >> coma >> array[i].natalicio >> coma >> array[i].estado >> coma >> array[i].id_os;
+        pac >> *array[i].DNI >> coma >> *array[i].nombre >> coma >> *array[i].apellido >> coma 
+        >> *array[i].sexo >> coma >> *array[i].natalicio >> coma >> *array[i].estado >> coma >> *array[i].id_os.obra_social;
         i++;
     }  //lee todo el archivo de paciente y lo guarda en el array dinamico
-    
-    Contacto *ArrContacto = new Contacto[N];
+
+
     string aux, aux2, aux3, aux4;
+    obs >> dummy >> coma >> dummy; //leo el header de IRI_ObraSocial.csv
+    
+    while(obs){
+        obs >> dummy >> coma >> aux; 
+        if(*array[i].id_os.id_obs == dummy) //si son la misma obra social (adaptado a que en la lista de pac tiene el numero, no el string)
+        {
+                *array[i].id_os.obra_social=aux;
+        }
+        i++;
+    } //lee todo el archivo de obra social y lo guarda en el array dinamico
+    
 
     cont >> dummy >> coma >> dummy >> coma >> dummy >> coma >> dummy >> coma >> dummy; //leo el header de IRI_Contactos.csv
 
     while(cont){
-        cont >> ArrContacto[i].DNI >> coma >> ArrContacto[i].telefono >> coma >> ArrContacto[i].celular >> coma >> ArrContacto[i].direccion >> coma >> ArrContacto[i].mail;
-        i++;
+        cont >> dummy >> coma >> aux >> aux2 >> coma >> aux3 >> coma >> aux4; 
+        if(*array[i].DNI == dummy) //si son la misma obra social
+        {
+                *array[i].contacto.telefono = aux;
+                *array[i].contacto.celular = aux2;
+                *array[i].contacto.direccion = aux3;
+                *array[i].contacto.mail = aux4;
         }
-        //lee todo el archivo de contactos y lo guarda en el array dinamico
+        i++;
+    } //lee todo el archivo de contactos y lo guarda en el array dinamico
     
    //falta medico y ultima consulta, hay que ver lo de las fechas como leerlas y ver si es necesario leer 
    //todas las consultas (hacemos array) o solo la ultima (convertimos todo a numero y buscamos el maximo)
 
+
     cons.close();
     cont.close();
     med.close();
+    obs.close();
     pac.close();
 
-    for(i=0; i<N; i++){
+    for(i=0; i<*N; i++){
        
        //hacer el calculo de los 10 años
 
-        if(array[i].estado == "fallecido"){//|| //dif tiempo > 10
+        if(*array[i].estado == "fallecido"){//|| //dif tiempo > 10
         
                i=3; //archivar (el i=3 esta de decoracion para que no aparezca un error)
         }
 
-        else if(array[i].estado == "internado"){ // || tiene turno a futuro
+        else if(*array[i].estado == "internado"){ // || tiene turno a futuro
         
             i=3;    //mover a activo y retornar (idem)
         }
 
         else{ //los que tenemos que contactar
-                int j; //para las listas nuevas
-                int k;
-                int l;
+                int *j = new int; //para las listas nuevas
+                int *k = new int;
+                int *l = new int;
+
+                bool respuesta = false;
+                string aux_os;
 
                 int answer = Contactar();
                 switch(answer){
@@ -109,8 +134,15 @@ int main() {
                                 //update, movemos al archivo
                                 break;
                         
-                        case 3: //quiere volver
-                                //update, movemos al activo
+                        case 3:         
+                                cout<<"necesita hacer un cambio en los datos de su obra social?: ";
+                                cin>>respuesta;
+                                if (respuesta == true)
+                                {
+                                cout<<"ingrese su nueva obra social: ";
+                                cin>>aux_os;
+                                Paciente.id_os[i] = aux_os;
+                                }
                                 break;
                         
                         case 4: //not found
