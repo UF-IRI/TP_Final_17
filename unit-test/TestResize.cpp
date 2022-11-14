46f6fc40cd3079f60cc8c2280560bdf340c195f4
SFiterman <ing.solfiterman@gmail.com>
2022-11-14 09:28:11 -0300

    unit-test resize

--- unit-test/TestResize.cpp
+++ unit-test/TestResize.cpp
@@ -2,7 +2,7 @@
 #include "resize.h"
 
 namespace Casos_Base {
-	TEST(Casos_Base, FuncionResize) {
+	TEST(Casos_Base, ResizePacientes) {
 		//declaro 2 arrays el de aca y el que testeo 
      
 		Paciente array[3];
@@ -71,11 +71,115 @@ namespace Casos_Base {
 		
 	}
 
-	TEST(Casos_Base, Test_Caso2) {
-		ASSERT_THAT(1, 1);
+	TEST(Casos_Base, ResizeContactos) {
+     
+		Contacto array[3];
+
+        array[0].DNI = "967597385";
+        array[0].telefono = "+41 203 339 0504";
+        array[0].celular = "+57 722 907 7201";
+        array[0].direccion = "Pine View";
+        array[0].mail = "strillow0@is.gd";
+
+        array[1].DNI = "398310454";
+        array[1].telefono = "+86 913 129 4605";
+        array[1].celular = "+62 268 398 6125";
+        array[1].direccion = "Lakewood Gardens";
+        array[1].mail = "hsubhan1@marketwatch.com";
+
+        array[2].DNI = "747423510";
+        array[2].telefono = "+1 696 710 1121";
+        array[2].celular = "+1 619 549 4324";
+        array[2].direccion = "Thackeray";
+        array[2].mail = "mbeaty2@ucoz.ru";
+
+        int N = 0;
+        Contacto* test= new Contacto[N];
+        
+        resizeCont(test, N, 2);
+       
+        test[0].DNI = "967597385";
+        test[0].telefono = "+41 203 339 0504";
+        test[0].celular = "+57 722 907 7201";
+        test[0].direccion = "Pine View";
+        test[0].mail = "strillow0@is.gd";
+
+        test[1].DNI = "398310454";
+        test[1].telefono = "+86 913 129 4605";
+        test[1].celular = "+62 268 398 6125";
+        test[1].direccion = "Lakewood Gardens";
+        test[1].mail = "hsubhan1@marketwatch.com";
+
+        test[2].DNI = "747423510";
+        test[2].telefono = "+1 696 710 1121";
+        test[2].celular = "+1 619 549 4324";
+        test[2].direccion = "Thackeray";
+        test[2].mail = "mbeaty2@ucoz.ru";
+
+        for(int i = 0; i < 3; i++){
+            EXPECT_EQ(array[i].DNI, test[i].DNI);    
+            EXPECT_EQ(array[i].telefono, test[i].telefono);    
+            EXPECT_EQ(array[i].celular, test[i].celular);    
+            EXPECT_EQ(array[i].direccion, test[i].direccion);    
+            EXPECT_EQ(array[i].mail, test[i].mail);        
+        }
+
 	}
 
-	TEST(Casos_Base, Test_Caso3) {
-		ASSERT_THAT(1, 1);
+	TEST(Casos_Base, ResizeConsultas) {
+       
+        Consultas array[3];
+
+        array[0].DNI = "432584544";
+        array[0].fecha_solicitado = 01/11/2001;
+        array[0].fecha_turno = 19/12/2001;
+        array[0].presento = true;
+        array[0].medico.matricula = "00-334-0838";
+
+        array[1].DNI = "867584849";
+        array[1].fecha_solicitado = 26/01/2009;
+        array[1].fecha_turno = 20/02/2009;
+        array[1].presento = true;
+        array[1].medico.matricula = "23-279-3287";
+       
+        array[2].DNI = "692767686";
+        array[2].fecha_solicitado = 16/06/1993;
+        array[2].fecha_turno = 14/07/1993;
+        array[2].presento = false;
+        array[2].medico.matricula = "12-340-2259";
+
+        int N = 0;
+        Consultas* test= new Consultas[N];
+        
+        resizeCons(test, N, 2);
+
+        test[0].DNI = "432584544";
+        test[0].fecha_solicitado = 01/11/2001;
+        test[0].fecha_turno = 19/12/2001;
+        test[0].presento = true;
+        test[0].medico.matricula = "00-334-0838";
+
+        test[1].DNI = "867584849";
+        test[1].fecha_solicitado = 26/01/2009;
+        test[1].fecha_turno = 20/02/2009;
+        test[1].presento = true;
+        test[1].medico.matricula = "23-279-3287";
+       
+        test[2].DNI = "692767686";
+        test[2].fecha_solicitado = 16/06/1993;
+        test[2].fecha_turno = 14/07/1993;
+        test[2].presento = false;
+        test[2].medico.matricula = "12-340-2259";
+
+        for(int i = 0; i < 3; i++){
+            EXPECT_EQ(array[i].DNI, test[i].DNI);    
+            EXPECT_EQ(array[i].fecha_solicitado, test[i].fecha_solicitado);    
+            EXPECT_EQ(array[i].fecha_turno, test[i].fecha_turno);    
+            EXPECT_EQ(array[i].presento, test[i].presento);    
+            EXPECT_EQ(array[i].medico.matricula, test[i].medico.matricula);        
+        }
+
+	
 	}
 }
+
